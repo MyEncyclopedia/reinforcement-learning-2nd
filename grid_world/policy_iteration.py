203bf8ffd3ea1e002859d14a809c1384c359c5e7
Frank <dingding303@gmail.com>
2020-09-04 20:47:42 +0800

    policy iteration

--- grid_world/policy_iteration.py
+++ grid_world/policy_iteration.py
@@ -1,7 +1,75 @@
-from mdp import MDP, Policy
+from GridWorldEnv import GridWorldEnv
+from policy_eval import policy_evaluate
+import numpy as np
 
 
-def policy_improvement(mdp: MDP, policy: Policy):
-    policy_stable = True
+def policy_improvement(env, gamma=1.0):
+    def one_step_lookahead(state, V):
+        """
+        Helper function to calculate the value for all action in a given state.
 
-    for s in range(1, 4 * 4 - 1):
+        Args:
+            state: The state to consider (int)
+            V: The value to use as an estimator, Vector of length env.nS
+
+        Returns:
+            A vector of length env.nA containing the expected value of each action.
+        """
+        A = np.zeros(env.nA)
+        for a in range(env.nA):
+            for prob, next_state, reward, done in env.P[state][a]:
+                A[a] += prob * (reward + gamma * V[next_state])
+        return A
+
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+
+    while True:
+        # Evaluate the current policy
+        V = policy_evaluate(policy, env, gamma)
+
+        # Will be set to false if we make any changes to the policy
+        policy_stable = True
+
+        # For each state...
+        for s in range(env.nS):
+            # The best action we would take under the current policy
+            chosen_a = np.argmax(policy[s])
+
+            # Find the best action by one-step lookahead
+            # Ties are resolved arbitarily
+            action_values = one_step_lookahead(s, V)
+            best_a = np.argmax(action_values)
+
+            # Greedily update the policy
+            if chosen_a != best_a:
+                policy_stable = False
+            policy[s] = np.eye(env.nA)[best_a]
+
+        # If the policy is stable we've found an optimal policy. Return it
+        if policy_stable:
+            return policy, V
+
+
+if __name__ == '__main__':
+    env = GridWorldEnv()
+
+    policy, v = policy_improvement(env)
+    print("Policy Probability Distribution:")
+    print(policy)
+    print("")
+
+    print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+    print(np.reshape(np.argmax(policy, axis=1), env.shape))
+    print("")
+
+    print("Value Function:")
+    print(v)
+    print("")
+
+    print("Reshaped Grid Value Function:")
+    print(v.reshape(env.shape))
+    print("")
+
+
+
