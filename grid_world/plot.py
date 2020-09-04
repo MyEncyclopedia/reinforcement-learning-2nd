6a6da3f6974945e59224c3b9a5cf543b5ced4fbc
Frank <dingding303@gmail.com>
2020-09-04 23:16:56 +0800

    plot

--- grid_world/plot.py
+++ grid_world/plot.py
@@ -1,82 +1,37 @@
 import numpy as np
 
+
 def matplot_bar3d_ex(V):
     import numpy as np
     import matplotlib.pyplot as plt
-    # This import registers the 3D projection, but is otherwise unused.
     from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
     # setup the figure and axes
-    fig = plt.figure(figsize=(8, 3))
-    ax1 = fig.add_subplot(121, projection='3d')
-    ax2 = fig.add_subplot(122, projection='3d')
+    fig = plt.figure(figsize=(6, 6))
+    ax = Axes3D(fig)
 
-    # fake data
     _x = np.arange(4)
     _y = np.arange(4)
     _xx, _yy = np.meshgrid(_x, _y)
     x, y = _xx.ravel(), _yy.ravel()
 
-    top = [x + 22 for x in V]
-    bottom = np.zeros_like(top)
+    Z = np.array([x  for x in V])
+    bottom = np.zeros_like(Z)
     width = depth = 1
 
-    ax1.bar3d(x, y, bottom, width, depth, top, shade=True)
-    ax1.set_title('Shaded')
+    colours = plt.cm.rainbow_r(Z / -22)
+    ax.bar3d(x, y, bottom, width, depth, Z, shade=True, color=colours)
+    ax.set_title('')
+    ax.set_zlim(-30, 1.01)
+    ax.set_xticks(np.arange(0, 4))
+    ax.set_yticks(np.arange(0, 4))
 
-    # ax2.bar3d(x, y, bottom, width, depth, top, shade=False)
-    # ax2.set_title('Not Shaded')
+    colour_map = plt.cm.ScalarMappable(cmap=plt.cm.rainbow_r)
+    colour_map.set_array(-Z)
+
+    col_bar = plt.colorbar(colour_map).set_label('')
 
     plt.show()
-
-
-def plotly_example():
-    import plotly_express as px
-    import plotly
-    import plotly.graph_objs as go
-    plotly.offline.init_notebook_mode(connected=True)
-
-    iris = px.data.iris()
-
-    iris_plot = px.scatter(iris, x='sepal_width', y='sepal_length',
-                           color='species', marginal_y='histogram',
-                           marginal_x='box', trendline='ols')
-
-    plotly.offline.plot(iris_plot)
-
-
-def plot_v(V):
-    import plotly_express as px
-    import plotly
-    import plotly.graph_objects as go
-    plotly.offline.init_notebook_mode(connected=True)
-    import numpy as np
-
-    fig = go.Figure(go.Surface(
-        # contours={
-        #     "x": {"show": True, "start": 1.5, "end": 2, "size": 0.04, "color": "white"},
-        #     "z": {"show": True, "start": 0.5, "end": 0.8, "size": 0.05}
-        # },
-        x=list(range(0, 4)),
-        y=list(range(0, 4)),
-        z=np.array(V).reshape(4, 4)
-        # z=[
-        #     [0, 1, 0, 1, 0],
-        #     [1, 0, 1, 0, 1],
-        #     [0, 1, 0, 1, 0],
-        #     [1, 0, 1, 0, 1],
-        #     [0, 1, 0, 1, 0]
-        # ]
-    ))
-    # fig.update_layout(
-    #     scene={
-    #         "xaxis": {"nticks": 20},
-    #         "zaxis": {"nticks": 4},
-    #         'camera_eye': {"x": 0, "y": -1, "z": 0.5},
-    #         "aspectratio": {"x": 1, "y": 1, "z": 0.2}
-    #     })
-    plotly.offline.plot(fig)
-    # fig.show()
 
 
 if __name__ == "__main__":
