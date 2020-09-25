b1ca6a169690079294c1f630474102a9edabc57d
Frank <dingding303@gmail.com>
2020-09-25 23:31:11 +0800

    blackjack mc predict very visit

--- blackjack/mc_predict_every_visit.py
+++ blackjack/mc_predict_every_visit.py
@@ -32,7 +32,7 @@ def gen_episode_data(policy: DeterministicPolicy, env: BlackjackEnv) -> List[Tup
     return episode_history
 
 
-def mc_prediction_first_visit(policy: DeterministicPolicy, env: BlackjackEnv,
+def mc_prediction_every_visit(policy: DeterministicPolicy, env: BlackjackEnv,
                               num_episodes, discount_factor=1.0) -> StateValue:
     returns_sum = defaultdict(float)
     returns_count = defaultdict(float)
@@ -44,9 +44,8 @@ def mc_prediction_first_visit(policy: DeterministicPolicy, env: BlackjackEnv,
         for t in range(len(episode_history) - 1, -1, -1):
             s, a, r = episode_history[t]
             G = discount_factor * G + r
-            if not any(s_a_r[0] == s for s_a_r in episode_history[0: t]):
-                returns_sum[s] += G
-                returns_count[s] += 1.0
+            returns_sum[s] += G
+            returns_count[s] += 1.0
 
     V = defaultdict(float)
     V.update({s: returns_sum[s] / returns_count[s] for s in returns_sum.keys()})
@@ -61,10 +60,11 @@ def fixed_policy(observation):
 
 if __name__ == "__main__":
     env = BlackjackEnv()
-    num_episodes = 10000
-    V = mc_prediction_first_visit(fixed_policy, env, num_episodes=num_episodes)
 
-    plot_value_function(V, title=f'First Visit {num_episodes} Steps')
+    num_episodes = 10000
+    V = mc_prediction_every_visit(fixed_policy, env, num_episodes=num_episodes)
+
+    plot_value_function(V, title=f'Every Visit {num_episodes} Steps')
 
     # V_500k = mc_prediction(sample_policy, env, num_episodes=50000)
     # plotting.plot_value_function(V_500k, title="500,000 Steps")
