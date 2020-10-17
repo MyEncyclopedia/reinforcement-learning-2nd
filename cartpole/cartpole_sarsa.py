509d8a1e1a50df17cae0799baf7897e447673af9
Frank <dingding303@gmail.com>
2020-10-17 12:09:12 +0800

    all three done

--- cartpole/cartpole_sarsa.py
+++ cartpole/cartpole_sarsa.py
@@ -28,6 +28,13 @@ class CartPoleAgent():
                      (-math.radians(50) / 1., math.radians(50) / 1., 12)]
         self.q = np.zeros(self.buckets + (self.env.action_space.n,))
 
+        self.pi = np.zeros_like(self.q)
+        for i in range(self.pi.shape[0]):
+            for a in range(self.env.action_space.n):
+                self.pi[i, a] = 1 / self.env.action_space.n
+        # print(self.pi)
+        print('done')
+
     def to_bin_idx(self, val: float, lower: float, upper: float, bucket_num: int) -> int:
         percent = (val + abs(lower)) / (upper - lower)
         return min(bucket_num - 1, max(0, int(round((bucket_num - 1) * percent))))
@@ -36,63 +43,71 @@ class CartPoleAgent():
         discrete_states = tuple([self.to_bin_idx(obs[d], *self.dims[d]) for d in range(len(obs))])
         return discrete_states
 
-    def choose_action(self, state) -> int:
+    def choose_action(self, state) -> Action:
         if np.random.random() < self.epsilon:
             return self.env.action_space.sample()
         else:
             return np.argmax(self.q[state])
 
-    def update_sarsa(self, state: State, action: Action, r, state_next: State, action_next: Action):
-        self.q[state][action] += self.lr * (r + self.discount * (self.q[state_next][action_next]) - self.q[state][action])
+    def update_sarsa(self, s: State, a: Action, r, s_next: State, a_next: Action):
+        self.q[s][a] += self.lr * (r + self.discount * (self.q[s_next][a_next]) - self.q[s][a])
 
-    def get_epsilon(self, t):
+    def update_q(self, s, a, r, s_next, a_next):
+        self.q[s][a] += self.lr * (r + self.discount * np.max(self.q[s_next]) - self.q[s][a])
+
+    def update_exp(self, s, a, r, s_next, a_next):
+        # print(np.dot(self.pi[s_next, :], self.q[s_next, :]))
+        # print(np.dot(self.pi[s_next], self.q[s_next]))
+        self.q[s][a] = self.q[s][a] + self.lr * (r + self.discount * np.dot(self.pi[s_next], self.q[s_next]) - self.q[s][a])
+        best_a = np.random.choice(np.where(self.q[s] == max(self.q[s]))[0])
+        n_actions = self.env.action_space.n
+        for i in range(n_actions):
+            if i == best_a:
+                self.pi[s][i] = 1 - (n_actions - 1) * (self.epsilon / n_actions)
+            else:
+                self.pi[s][i] = self.epsilon / n_actions
+
+    def adjust_epsilon(self, t) -> float:
         self.epsilon = max(self.min_epsilon, self.epsilon * 0.99)
         return self.epsilon
-        # return max(self.min_epsilon, min(1., 1. - math.log10((t + 1) / self.decay)))
 
-    def get_learning_rate(self, t):
-        # return 0.05
-        # return self.min_lr
-        # return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
+    def adjust_learning_rate(self, t) -> float:
         self.lr = max(self.min_lr, self.lr * 0.99)
-        print(self.lr)
         return self.lr
 
-        # return self.lr * 0.95
-
-
     def train(self):
-
         for e in range(self.num_episodes):
             print(e)
             s: State = self.discretize(self.env.reset())
 
-            self.lr = self.get_learning_rate(e)
-            self.epsilon = self.get_epsilon(e)
+            self.adjust_learning_rate(e)
+            self.adjust_epsilon(e)
             done = False
 
             while not done:
-                action = self.choose_action(s)
+                action: Action = self.choose_action(s)
                 obs, reward, done, _ = self.env.step(action)
-                s_prime: State = self.discretize(obs)
-                a_prime = self.choose_action(s_prime)
-                self.update_sarsa(s, action, reward, s_prime, a_prime)
-                s = s_prime
+                s_next: State = self.discretize(obs)
+                a_next = self.choose_action(s_next)
+                # self.update_sarsa(s, action, reward, s_next, a_next)
+                # self.update_q(s, action, reward, s_next, a_next)
+                self.update_exp(s, action, reward, s_next, a_next)
+                s = s_next
 
         print('Finished training!')
 
-    def run(self):
+    def test(self):
         self.env = gym.wrappers.Monitor(self.env, 'cartpole')
         t = 0
         done = False
-        current_state = self.discretize(self.env.reset())
+        s: State = self.discretize(self.env.reset())
         while not done:
             self.env.render()
-            t = t + 1
-            action = self.choose_action(current_state)
+            t += 1
+            action: Action = self.choose_action(s)
             obs, reward, done, _ = self.env.step(action)
-            new_state = self.discretize(obs)
-            current_state = new_state
+            s_next = self.discretize(obs)
+            s = s_next
 
         return t
 
@@ -101,5 +116,5 @@ if __name__ == "__main__":
     agent = CartPoleAgent()
     agent.train()
     for _ in range(5):
-        t = agent.run()
+        t = agent.test()
         print("Time", t)
