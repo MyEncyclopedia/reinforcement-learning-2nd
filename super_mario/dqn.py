53f3fbdda85ea0ba7ce8bc9bc05e198cfe421dd8
Frank Ding <dingding303@gmail.com>
2020-12-04 21:24:01 +0800

    dqn

--- super_mario/dqn.py
+++ super_mario/dqn.py
@@ -10,8 +10,8 @@ from gym_super_mario_bros.actions import COMPLEX_MOVEMENT, RIGHT_ONLY, SIMPLE_MO
 from torch import save, FloatTensor, LongTensor
 from torch.optim import Adam
 
-from .wrappers import wrap_environment
-from .model import CNNDQN
+from super_mario.wrappers import wrap_environment
+from super_mario.model import CNNDQN
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'done'))
 
@@ -43,7 +43,7 @@ class ReplayMemory:
         return len(self.memory)
 
 
-ENVIRONMENT = 'SuperMarioBros-1-1-v0'
+ENV_NAME = 'SuperMarioBros-1-1-v0'
 BATCH_SIZE = 32
 BETA_FRAMES = 10000
 BETA_START = 0.4
@@ -58,14 +58,12 @@ NUM_EPISODES = 50000
 TARGET_UPDATE_FREQUENCY = 1000
 
 
-def update_graph(model, target_model, optimizer, replay_buffer, args, device,
-                 episode_idx, beta):
-    if len(replay_buffer) > args.initial_learning:
+def update_graph(model, target_model, optimizer, replay_mem: ReplayMemory, args, device, episode_idx):
+    if len(replay_mem) > args.initial_learning:
         if not episode_idx % args.target_update_frequency:
             target_model.load_state_dict(model.state_dict())
         optimizer.zero_grad()
-        compute_td_loss(model, target_model, replay_buffer, args.gamma, device,
-                        args.batch_size, beta)
+        compute_td_loss(model, target_model, replay_mem, args.gamma, device, args.batch_size)
         optimizer.step()
 
 
@@ -79,63 +77,31 @@ def exercise_new_model(model, environment, info, action_space):
     #                                          environment))
 
 
-def complete_episode(model, environment, episode_reward, episode_idx, epsilon, stats, action_space):
-    pass
-    # new_best = info.update_rewards(episode_reward)
-    # if new_best:
-    #     print('New best average reward of %s! Saving model'
-    #           % round(info.best_average, 3))
-    #     exercise_new_model(model, environment, info, action_space)
-    # elif stats['flag_get']:
-    #     info.update_best_counter()
-    #     exercise_new_model(model, environment, info, action_space)
-    # print('Episode %s - Reward: %s, Best: %s, Average: %s '
-    #       'Epsilon: %s' % (episode,
-    #                        round(episode_reward, 3),
-    #                        round(info.best_reward, 3),
-    #                        round(info.average, 3),
-    #                        round(epsilon, 4)))
-
-
-
-
-def train(env, model, target_model, optimizer, replay_buffer, args, device):
+def train(env, model, target_model, optimizer, replay_mem: ReplayMemory, args, device):
     for episode_idx in range(args.num_episodes):
         episode_reward = 0.0
         state = env.reset()
 
         while True:
             epsilon = update_epsilon(episode_idx, args)
-            if len(replay_buffer) > args.batch_size:
-                beta = update_beta(episode_idx, args)
-            else:
-                beta = args.beta_start
             action = model.act(state, epsilon, device)
             if args.render:
                 env.render()
             next_state, reward, done, stats = env.step(action)
-            replay_buffer.push(state, action, reward, next_state, done)
+            replay_mem.push(state, action, reward, next_state, done)
             state = next_state
             episode_reward += reward
-            update_graph(model, target_model, optimizer, replay_buffer, args, device, episode_idx, beta)
+            update_graph(model, target_model, optimizer, replay_mem, args, device, episode_idx)
             if done:
-                complete_episode(model, args.environment, episode_reward,
-                                 episode_idx, epsilon, stats, args.action_space)
+                print(f'{episode_idx}: {episode_reward}')
                 break
 
 
 def parse_args():
     parser = ArgumentParser(description='')
     parser.add_argument('--batch-size', type=int, default=BATCH_SIZE)
-    parser.add_argument('--beta-frames', type=int, help='The number of frames '
-                        'to update the beta value before reaching the maximum '
-                        'of 1.0. Default: %s' % BETA_FRAMES,
-                        default=BETA_FRAMES)
-    parser.add_argument('--beta-start', type=float, help='The initial value '
-                        'of beta to be used in the prioritized replay. '
-                        'Default: %s' % BETA_START, default=BETA_START)
     parser.add_argument('--buffer-capacity', type=int, default=MEMORY_CAPACITY)
-    parser.add_argument('--environment', type=str, default=ENVIRONMENT)
+    parser.add_argument('--env-name', type=str, default=ENV_NAME)
     parser.add_argument('--epsilon-start', type=float, choices=[Range(0.0, 1.0)], default=EPSILON_START, metavar='EPSILON_START')
     parser.add_argument('--epsilon-final', type=float, choices=[Range(0.0, 1.0)], default=EPSILON_FINAL, metavar='EPSILON_FINAL')
     parser.add_argument('--epsilon-decay', type=int, default=EPSILON_DECAY)
@@ -148,9 +114,8 @@ def parse_args():
 
     return parser.parse_args()
 
-def compute_td_loss(model, target_net, replay_buffer, gamma, device,
-                    batch_size, beta):
-    batch = replay_buffer.sample(batch_size, beta)
+def compute_td_loss(model, target_net, replay_mem: ReplayMemory, gamma, device, batch_size):
+    batch = replay_mem.sample(batch_size)
     state, action, reward, next_state, done, indices, weights = batch
 
     state = Variable(FloatTensor(np.float32(state))).to(device)
@@ -171,7 +136,6 @@ def compute_td_loss(model, target_net, replay_buffer, gamma, device,
     prios = loss + 1e-5
     loss = loss.mean()
     loss.backward()
-    replay_buffer.update_priorities(indices, prios.data.cpu().numpy())
 
 
 def update_epsilon(episode, args):
@@ -182,23 +146,17 @@ def update_epsilon(episode, args):
     return epsilon
 
 
-def update_beta(episode, args):
-    start = args.beta_start
-    frames = args.beta_frames
-    beta = start + episode * (1.0 - start) / frames
-    return min(1.0, beta)
-
 
 def main():
     args = parse_args()
-    env = wrap_environment(args.environment, RIGHT_ONLY)
+    env = wrap_environment(args.env_name, RIGHT_ONLY)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model = CNNDQN(env.observation_space.shape, env.action_space.n).to(device)
     target_model = CNNDQN(env.observation_space.shape, env.action_space.n).to(device)
 
     optimizer = Adam(model.parameters(), lr=args.learning_rate)
-    replay_buffer = ReplayMemory(args.buffer_capacity)
-    train(env, model, target_model, optimizer, replay_buffer, args, device)
+    replay_mem = ReplayMemory(args.buffer_capacity)
+    train(env, model, target_model, optimizer, replay_mem, args, device)
     env.close()
 
 
