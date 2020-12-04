454a50fb08be77cb96c7c49f5396b47eaf5c0d26
Frank Ding <dingding303@gmail.com>
2020-12-04 21:11:52 +0800

    dqn

--- super_mario/dqn.py
+++ super_mario/dqn.py
@@ -43,11 +43,10 @@ class ReplayMemory:
         return len(self.memory)
 
 
-ACTION_SPACE = 'complex'
+ENVIRONMENT = 'SuperMarioBros-1-1-v0'
 BATCH_SIZE = 32
 BETA_FRAMES = 10000
 BETA_START = 0.4
-ENVIRONMENT = 'SuperMarioBros-1-1-v0'
 EPSILON_START = 1.0
 EPSILON_FINAL = 0.01
 EPSILON_DECAY = 100000
@@ -56,7 +55,6 @@ INITIAL_LEARNING = 10000
 LEARNING_RATE = 1e-4
 MEMORY_CAPACITY = 20000
 NUM_EPISODES = 50000
-PRETRAINED_MODELS = 'pretrained_models'
 TARGET_UPDATE_FREQUENCY = 1000
 
 
@@ -99,43 +97,36 @@ def complete_episode(model, environment, episode_reward, episode_idx, epsilon, s
     #                        round(epsilon, 4)))
 
 
-def run_episode(env, model, target_model, optimizer, replay_buffer, args,
-                device, episode_idx):
-    episode_reward = 0.0
-    state = env.reset()
-
-    while True:
-        epsilon = update_epsilon(episode_idx, args)
-        if len(replay_buffer) > args.batch_size:
-            beta = update_beta(episode_idx, args)
-        else:
-            beta = args.beta_start
-        action = model.act(state, epsilon, device)
-        if args.render:
-            env.render()
-        next_state, reward, done, stats = env.step(action)
-        replay_buffer.push(state, action, reward, next_state, done)
-        state = next_state
-        episode_reward += reward
-        update_graph(model, target_model, optimizer, replay_buffer, args,
-                     device, episode_idx, beta)
-        if done:
-            complete_episode(model, args.environment, episode_reward,
-                             episode_idx, epsilon, stats, args.action_space)
-            break
 
 
 def train(env, model, target_model, optimizer, replay_buffer, args, device):
-    for episode in range(args.num_episodes):
-        run_episode(env, model, target_model, optimizer, replay_buffer, args,
-                    device, episode)
+    for episode_idx in range(args.num_episodes):
+        episode_reward = 0.0
+        state = env.reset()
+
+        while True:
+            epsilon = update_epsilon(episode_idx, args)
+            if len(replay_buffer) > args.batch_size:
+                beta = update_beta(episode_idx, args)
+            else:
+                beta = args.beta_start
+            action = model.act(state, epsilon, device)
+            if args.render:
+                env.render()
+            next_state, reward, done, stats = env.step(action)
+            replay_buffer.push(state, action, reward, next_state, done)
+            state = next_state
+            episode_reward += reward
+            update_graph(model, target_model, optimizer, replay_buffer, args, device, episode_idx, beta)
+            if done:
+                complete_episode(model, args.environment, episode_reward,
+                                 episode_idx, epsilon, stats, args.action_space)
+                break
 
 
 def parse_args():
     parser = ArgumentParser(description='')
-    parser.add_argument('--batch-size', type=int, help='Specify the batch '
-                        'size to use when updating the replay buffer. '
-                        'Default: %s' % BATCH_SIZE, default=BATCH_SIZE)
+    parser.add_argument('--batch-size', type=int, default=BATCH_SIZE)
     parser.add_argument('--beta-frames', type=int, help='The number of frames '
                         'to update the beta value before reaching the maximum '
                         'of 1.0. Default: %s' % BETA_FRAMES,
@@ -143,61 +134,18 @@ def parse_args():
     parser.add_argument('--beta-start', type=float, help='The initial value '
                         'of beta to be used in the prioritized replay. '
                         'Default: %s' % BETA_START, default=BETA_START)
-    parser.add_argument('--buffer-capacity', type=int, help='The capacity to '
-                        'use in the experience replay buffer. Default: %s'
-                        % MEMORY_CAPACITY, default=MEMORY_CAPACITY)
-    parser.add_argument('--environment', type=str, help='The OpenAI gym '
-                        'environment to use. Default: %s' % ENVIRONMENT,
-                        default=ENVIRONMENT)
-    parser.add_argument('--epsilon-start', type=float, help='The initial '
-                        'value for epsilon to be used in the epsilon-greedy '
-                        'algorithm. Default: %s' % EPSILON_START,
-                        choices=[Range(0.0, 1.0)], default=EPSILON_START,
-                        metavar='EPSILON_START')
-    parser.add_argument('--epsilon-final', type=float, help='The final value '
-                        'for epislon to be used in the epsilon-greedy '
-                        'algorithm. Default: %s' % EPSILON_FINAL,
-                        choices=[Range(0.0, 1.0)], default=EPSILON_FINAL,
-                        metavar='EPSILON_FINAL')
-    parser.add_argument('--epsilon-decay', type=int, help='The decay factor '
-                        'for epsilon in the epsilon-greedy algorithm. '
-                        'Default: %s' % EPSILON_DECAY, default=EPSILON_DECAY)
-    parser.add_argument('--force-cpu', action='store_true', help='By default, '
-                        'the program will run on the first supported GPU '
-                        'identified by the system, if applicable. If a '
-                        'supported GPU is installed, but all computations are '
-                        'desired to run on the CPU only, specify this '
-                        'parameter to ignore the GPUs. All actions will run '
-                        'on the CPU if no supported GPUs are found. Default: '
-                        'False')
-    parser.add_argument('--gamma', type=float, help='Specify the discount '
-                        'factor, gamma, to use in the Q-table formula. '
-                        'Default: %s' % GAMMA, choices=[Range(0.0, 1.0)],
-                        default=GAMMA, metavar='GAMMA')
-    parser.add_argument('--initial-learning', type=int, help='The number of '
-                        'iterations to explore prior to updating the model '
-                        'and begin the learning process. Default: %s'
-                        % INITIAL_LEARNING, default=INITIAL_LEARNING)
-    parser.add_argument('--learning-rate', type=int, help='The learning rate '
-                        'to use for the optimizer. Default: %s'
-                        % LEARNING_RATE, default=LEARNING_RATE)
-    parser.add_argument('--num-episodes', type=int, help='The number of '
-                        'episodes to run in the given environment. Default: '
-                        '%s' % NUM_EPISODES, default=NUM_EPISODES)
-    parser.add_argument('--render', action='store_true', help='Specify to '
-                        'render a visualization in another window of the '
-                        'learning process. Note that a Desktop Environment is '
-                        'required for visualization. Rendering scenes will '
-                        'lower the learning speed. Default: False')
-    parser.add_argument('--target-update-frequency', type=int, help='Specify '
-                        'the number of iterations to run prior to updating '
-                        'target network with the primary network\'s weights. '
-                        'Default: %s' % TARGET_UPDATE_FREQUENCY,
-                        default=TARGET_UPDATE_FREQUENCY)
-    parser.add_argument('--transfer', action='store_true', help='Transfer '
-                        'model weights from a previously-trained model to new '
-                        'models for faster learning and improved accuracy. '
-                        'Default: False')
+    parser.add_argument('--buffer-capacity', type=int, default=MEMORY_CAPACITY)
+    parser.add_argument('--environment', type=str, default=ENVIRONMENT)
+    parser.add_argument('--epsilon-start', type=float, choices=[Range(0.0, 1.0)], default=EPSILON_START, metavar='EPSILON_START')
+    parser.add_argument('--epsilon-final', type=float, choices=[Range(0.0, 1.0)], default=EPSILON_FINAL, metavar='EPSILON_FINAL')
+    parser.add_argument('--epsilon-decay', type=int, default=EPSILON_DECAY)
+    parser.add_argument('--gamma', type=float, choices=[Range(0.0, 1.0)], default=GAMMA, metavar='GAMMA')
+    parser.add_argument('--initial-learning', type=int, default=INITIAL_LEARNING)
+    parser.add_argument('--learning-rate', type=int, default=LEARNING_RATE)
+    parser.add_argument('--num-episodes', type=int, default=NUM_EPISODES)
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument('--target-update-frequency', type=int, default=TARGET_UPDATE_FREQUENCY)
+
     return parser.parse_args()
 
 def compute_td_loss(model, target_net, replay_buffer, gamma, device,
@@ -241,19 +189,10 @@ def update_beta(episode, args):
     return min(1.0, beta)
 
 
-def set_device(force_cpu):
-    # force_cpu = True
-    device = torch.device('cpu')
-    if not force_cpu and torch.cuda.is_available():
-        device = torch.device('cuda')
-    print(device)
-    return device
-
-
 def main():
     args = parse_args()
     env = wrap_environment(args.environment, RIGHT_ONLY)
-    device = set_device(args.force_cpu)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model = CNNDQN(env.observation_space.shape, env.action_space.n).to(device)
     target_model = CNNDQN(env.observation_space.shape, env.action_space.n).to(device)
 
